03fef69d0b9494c76fc49c76dc583795943e8c12
Faris Haidar Zuhdi <rastasonna@gmail.com>
2026-03-24 23:36:15 +0700

    fix: support SQLITE_DB_PATH env var for k8s volume mount

--- irnuscrawler/settings.py
+++ irnuscrawler/settings.py
@@ -80,7 +80,7 @@ ASGI_APPLICATION = 'irnuscrawler.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('SQLITE_DB_PATH', str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
